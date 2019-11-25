ddb43490c457a10fcd0c8cca696966d6409dba2a
github username <github email address>
2019-11-25 11:48:03 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Quotes from './containers/Quotes'
+import QuoteForm from './components/QuoteForm'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="container-fluid">
-        <div className="row title justify-content-center" style={{ paddingTop: '12px' }}>
+      <div className='container-fluid'>
+        <div
+          className='row title justify-content-center'
+          style={{ paddingTop: '12px' }}
+        >
           <h1>Quote Maker</h1>
         </div>
         <hr />
-        {/*
-          TODO:
-
-          Render QuoteForm and Quotes Components
-         */}
+        <QuoteForm />
+        <Quotes />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
-import { combineReducers } from 'redux';
-import quotes from './quotes';
+import { combineReducers } from 'redux'
+import quotesReducer from './quotes'
 
-export default combineReducers({
-  quotes
-});
+const rootReducer = combineReducers({
+  quotes: quotesReducer
+})
+
+export default rootReducer
